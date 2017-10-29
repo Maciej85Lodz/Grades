164b892477e9e2e6f9d77688f1f14797b98046c6
maciej.wolejszo <maciej.wolejszo@gmail.com>
2017-10-29 23:26:19 +0100

    Lesson 04 - Types; Reference Types

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -10,7 +10,7 @@ namespace Grades
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             
             GradeBook book = new GradeBook();
             book.AddGrade(91);
@@ -21,7 +21,7 @@ namespace Grades
             Console.WriteLine(stats.AverageGrade);
             Console.WriteLine(stats.HighestGrade);
             Console.WriteLine(stats.LowestGrade);
-            
+ 
         }
     }
 }
